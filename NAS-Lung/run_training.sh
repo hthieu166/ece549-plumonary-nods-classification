d7dafdd8dce5b0fa7429b59a86d76eef811f763b
hthieu166 <hthieu@selab.hcmus.edu.vn>
2021-03-30 08:15:06 +0700

    add nas model 2

--- NAS-Lung/run_training.sh
+++ NAS-Lung/run_training.sh
@@ -8,5 +8,5 @@ maxeps=9
 for (( i=0; i<=$maxeps; i+=1)) 
 do
     echo "process $i epoch"
-    python main.py --batch_size 8 --fold $i --config_file configs/dpn3d.yml
+    python main.py --batch_size 8 --fold $i --config_file configs/nas-model-2.yml
 done 
